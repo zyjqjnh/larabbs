45bcd3360927fe89a0a675b75d204b43c15828ba
祝垚杰 <zyjqjnh@126.com>
2019-01-04 01:28:40 +0000

    基础页面结构

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    </head>
+    <body>
+        <div id="app" class="{{ route_class() }}-page">
+            @include('layouts._header')
+            <div class="container">
+                @include('shared._messages')
+                @yield('content')
+            </div>
+            @include('layouts._footer')
+        </div>
+        <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+    </body>
+</html>
