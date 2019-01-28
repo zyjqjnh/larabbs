432eb8311dc35a179a838174a8fcdc2d45f20456
祝垚杰 <zyjqjnh@126.com>
2019-01-28 09:30:31 +0000

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        @yield('styles')
     </head>
     <body>
         <div id="app" class="{{ route_class() }}-page">
@@ -18,5 +19,6 @@
             @include('layouts._footer')
         </div>
         <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+        @yield('scripts')
     </body>
 </html>
