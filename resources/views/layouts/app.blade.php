0e098909f2150a1010effb4d89c9475eee26c52f
祝垚杰 <zyjqjnh@126.com>
2019-02-02 01:36:20 +0000

    话题内容页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+        <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         @yield('styles')
     </head>
