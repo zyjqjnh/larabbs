58210c2aa77f92c50ce11ad7cfa1c22eb9caf86b
祝垚杰 <zyjqjnh@126.com>
2019-02-11 02:50:19 +0000

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,8 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
